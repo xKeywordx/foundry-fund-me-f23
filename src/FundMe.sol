0bdc79f497801262a75260abf9ea07e4a5febd66
Keyword <111123692+xKeywordx@users.noreply.github.com>
2023-12-22 23:07:29 +0200

    Update FundMe.sol

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -12,8 +12,7 @@ contract FundMe {
     mapping(address => uint256) private s_addressToAmountFunded;
     address[] private s_funders;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address private /* immutable */ i_owner;
+    address private immutable i_owner;
     uint256 public constant MINIMUM_USD = 5e18;
     AggregatorV3Interface private s_priceFeed;
     
